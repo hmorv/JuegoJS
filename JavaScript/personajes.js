c659822980bc1215f552bd378f4cc069c8cff6d4
Adrian Martinez <ark.progr@gmail.com>
2016-05-10 20:17:40 +0200

    Añadido la funcion de ataque para los personajes

--- JavaScript/personajes.js
+++ JavaScript/personajes.js
@@ -15,14 +15,36 @@ function Personaje(tipo) {
 			this.Arma = "Mandoble de Hierro";
 			this.Lvl = 1;
 			this.Exp = 0;
+			break;
 		case "Nigromante":
 			this.Hp = 350;
 			this.Spells = ["Eclipse","Abismo","Pesadilla"];
 			this.Arma = "Libro de las Almas Condenadas";
 			this.Lvl = 1;
 			this.Exp = 0;
+			break;
 		default:
 			break;
+	}
+
+	this.Atacar = function() {
+		var op = prompt("Atacaras con tu Arma o con tus Spells");
+
+		if (op == "Arma") {
+			var daño = Math.floor((Math.random() * 100) + 1);
+			document.write("Atacas con tu " + this.Arma + " y haces " + daño + " ptos de daño!!<br/>");
+		} else {
+			if (this.Spells instanceof Array) {
+				var spell = prompt("Elige el Spell que utilizaras: " + this.Spells[0] + " , " + this.Spells[1] + " , " + this.Spells[2]);
+				var daño = Math.floor((Math.random() * 100) + 1);
+				document.write("Atacas con tu Spell: " + spell + " y haces " + daño + " ptos de daño!!<br/>"); 
+			} else {
+				var spell = prompt("Elige el Spell que utilizaras " + this.Spells);
+				var daño = Math.floor((Math.random() * 100) + 1);
+				document.write("Atacas con tu Spell: " + spell + " y haces " + daño + " ptos de daño!!<br/>"); 
+			}
+			
+		}
 	}
 } 
 
@@ -32,6 +54,6 @@ function MostrarSpells(persj) {
 			document.write(persj.Spells[i] + "<br/>");	
 		}
 	} else {
-		document.write(persj.Spells);
+		document.write(persj.Spells + "<br/>");
 	}
 }
